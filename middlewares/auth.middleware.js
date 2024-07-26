1bdceebb5ca1fe10cf523d9c69165094e5b01241
Sonuu20 <sonugupta0208b@gmail.com>
2024-07-26 13:32:42 +0530

    created middlewares

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import { getUser } from "../service/auth.service.js";
+
+async function restrictToLoggedinUserOnly(req, res, next) {
+    // console.log(req);
+  const userUid = req.cookies?.uid;
+
+  if (!userUid) return res.redirect("/login");
+  const user = getUser(userUid);
+
+  if (!user) return res.redirect("/login");
+
+  req.user = user;
+  next();
+}
+
+async function checkAuth(req, res, next) {
+  const userUid = req.cookies?.uid;
+
+  const user = getUser(userUid);
+
+  req.user = user;
+  next();
+}
+
+export { restrictToLoggedinUserOnly, checkAuth };
